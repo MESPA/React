ee5cdae90ea00d66edc052fae79bddc72289cad1
MESPA <manuel030888@gmail.com>
2020-11-29 22:27:26 -0400

    formHookValidaciones
    formHookValidaciones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Formulario from './component/Formulario'
-import Jsx from './component/JSX'
-import Contador from './component/JSX'
-import Lista from './component/Lista'
-
+import Formulario from './component/Formulario';
+import Jsx from './component/JSX';
+import Contador from './component/JSX';
+import Lista from './component/Lista';
+import FormHook from './component/FormHook';
 
 function App() {
   return (
     <div className = "container mt-5">
-  
+  <FormHook/>
    {/* <Formulario/> */}
    
     {/* < Jsx />
