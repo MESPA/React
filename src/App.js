480c4b01c6e0c4d2d70a84c532fc1b535ade1383
MESPA <manuel030888@gmail.com>
2020-11-30 22:25:46 -0400

    simplecrud
    simplecrud

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import Formulario from './component/Formulario';
-import Jsx from './component/JSX';
-import Contador from './component/JSX';
-import Lista from './component/Lista';
-import FormHook from './component/FormHook';
+import React, {useState} from 'react';
+// import Formulario from './component/Formulario';
+// import Jsx from './component/JSX';
+// import Contador from './component/JSX';
+// import Lista from './component/Lista';
+// import FormHook from './component/FormHook';
+import TablaUsuario from './CompontCrud/TablaUsuario';
+import AddUserForm  from './CompontCrud/AddUserForm ';
+import {v4 as uuidv4}  from 'uuid';
+
 
 function App() {
+  const usersData = [
+
+  { id:uuidv4(), name: 'Tania', username: 'floppydiskette' },
+  { id:uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+  { id:uuidv4(), name: 'Ben', username: 'benisphere' },
+]
+//state
+const [users, setUsers] = useState(usersData);
+//agregar usuario
+const addUser = (user) => {
+  user.id = uuidv4()
+  setUsers([
+    ...users,user
+
+  ])
+}
+
+//eliminar usuario
+const deleteUser = (id) => {
+  setUsers(users.filter((user) => user.id !== id))
+  console.log(id)
+}
   return (
     <div className = "container mt-5">
-  <FormHook/>
+  
+  <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Agregar usuario</h2>
+          <AddUserForm addUser={addUser} />
+        </div>
+        <div className="flex-large">
+          <h2>ver usuarios</h2>
+          <TablaUsuario users= {users} deleteUser= {deleteUser}/>
+        </div>
+      </div>
+    </div>
+  
+  
+  
+   {/* <FormHook/> */}
    {/* <Formulario/> */}
    
-    {/* < Jsx />
-    < Lista/> */}
-    {/* <Contador/> */}
+   {/* < Jsx />
+   < Lista/> */}
+  {/* <Contador/> */}
     </div>
   );
 }
---
+++ src/CompontCrud/AddUserForm .jsx
@@ -0,0 +1,44 @@
+import React from 'react'; 
+import { useForm } from 'react-hook-form' 
+
+const AddUserForm  = (props) => {
+ 
+    const {register , errors , handleSubmit}= useForm();
+    const onSubmit = (data,e )=>{
+        
+        console.log(data)
+
+        //recibir data del cmponente padre
+        props.addUser(data)
+        //limpiar campos
+        e.target.reset();
+    
+    }
+
+    return (  
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input type="text" name="name" ref={
+          register({
+              required:{value :true, message:'Campo Requerido'}
+          })
+      }  />
+      <span className="text-danger text-small d-block mb-2">
+    {errors?.name?.message}
+</span>
+      <label>Username</label>
+      <input type="text" name="username" ref={
+          register({
+              required:{value :true, message:'Campo Requerido'}
+          })
+      } />
+       <span className="text-danger text-small d-block mb-2">
+    {errors?.username?.message}
+</span>
+      <button>Add new user</button>
+    </form>
+    );
+}
+ 
+export default AddUserForm ;
